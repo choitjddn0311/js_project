49b52fee130658969516d27af95dbbf9bf01664b
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-04-14 01:08:57 +0900

    feat: add_click message and score board

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user_score");
 const computerScore_span = document.getElementById("computer_score");
 const scoreBoard_div = document.querySelector(".score_board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,13 +14,47 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-function game(userChoice) {
-    const computerChoice = getComputerChoice();
-    console.log("user choice => " + userChoice);
-    console.log("computer choice => " + computerChoice);
+function converToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
 }
 
-game("c");
+function win(userChoice , computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = converToWord(userChoice) + " beats " +  converToWord(computerChoice) + ". You win! 🔥";
+}
+
+function lose() {
+    console.log("LOSE")
+}
+
+function draw() {
+    console.log("DRAW")
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+          win(userChoice , computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+          lose(userChoice , computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+          draw(userChoice , computerChoice);
+            break;
+    }
+}
 
 function main () {
     rock_div.addEventListener('click', function() {
