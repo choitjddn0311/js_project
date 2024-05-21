a5cd29ac84384d67efaf9e6ec4932557add27e19
choitjddn0311 <cswcjw311427@naver.com>
2024-05-21 17:57:50 +0900

    docs : add link

--- my_project/src/app.js
+++ my_project/src/app.js
@@ -7,3 +7,5 @@ app.listen(3001 , () => {
 app.get('/' , (req , res) => {
     res.send('인평자동차고등학교');
 });
+
+// https://velog.io/@seo__namu/Express-%EA%B0%9C%EB%B0%9C-%ED%99%98%EA%B2%BD-%EA%B5%AC%EC%84%B1%ED%95%98%EA%B8%B0
