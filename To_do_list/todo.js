9c697cdbb0c6a83b946ed70e8fb7ccf94a074b96
choitjddn0311 <cswcjw311427@naver.com>
2024-05-27 22:45:15 +0900

    feat  : add click event

--- To_do_list/todo.js
+++ To_do_list/todo.js
@@ -1,7 +1,7 @@
 let text = document.getElementById("txt");
 let plus = document.getElementById("plus");
+let todo = document.getElementById("todo");
 
 plus.addEventListener('click' , ()=> {
-    // html에 ul 안에 li 생성
-    //text안에 있는글을 li에 넣는다
+    todo.innerHTML = "<li style= 'color: red;'>인평자동차고등학교</li>"
 })
