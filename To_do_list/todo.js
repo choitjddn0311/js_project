e345f626920733206e64501c9266ed7dcdfdf29c
choitjddn0311 <cswcjw311427@naver.com>
2024-05-27 16:25:43 +0900

    Docs : add comments

--- To_do_list/todo.js
+++ To_do_list/todo.js
@@ -2,5 +2,6 @@ let text = document.getElementById("txt");
 let plus = document.getElementById("plus");
 
 plus.addEventListener('click' , ()=> {
-    document.createElement("div")
+    // html에 ul 안에 li 생성
+    //text안에 있는글을 li에 넣는다
 })
