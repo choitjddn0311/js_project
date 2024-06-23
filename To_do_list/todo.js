9e28efa9fe7d871ae063ce5af8a247a40ef6e9c9
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-06-23 23:42:47 +0900

    feat : open an alert when the input value is empty

--- To_do_list/todo.js
+++ To_do_list/todo.js
@@ -15,13 +15,9 @@ plus.addEventListener('click' , ()=> {
     li.appendChild(del);
     todo.appendChild(li);
     text.value = '';
-    if (text === ' ') {
-           
+    if(text.value.trim() === '') {
+        alert("the values are empty , please enter the values");
+        return;
     }
 })
 
-/**
- * 해야할 것
- * 1) input에 입력한 후 엔터치면 추가되게 할것
- * 2) input 안에 값이 없을경우 추가가 되지않게 할것
- */
