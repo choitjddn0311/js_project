4ba1a98e27e2d43ef5d123b46adec4fecedba5b3
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-07-17 00:21:42 +0900

    feat : hangman game finish

--- Hangman_game/script/script.js
+++ Hangman_game/script/script.js
@@ -2,34 +2,57 @@ const hangmanImage = document.querySelector('.hangman-box img');
 const wordDisplay = document.querySelector('.word-display');
 const guessesText = document.querySelector('.guesses-text b');
 const keyboardDiv = document.querySelector('.keyboard');
+const gameModal = document.querySelector('.game-modal');
+const playAgainBtn = document.querySelector('.play-again');
 
-let currentWord, wrongGuessCount = 0;
+let currentWord, correctLetters = [], wrongGuessCount = 0;
 const maxGuesses = 6;
+
+const resetGame = () => {
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    gameModal.classList.remove("show");
+}
 
 const getRandomWord = () => {
     //wordList파일에서 랜덤 단어와 힌트를 선택한다
     const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
-    console.log(word);
     document.querySelector(".hint-text").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+    resetGame();
+}
+
+const gameOver = (isVictory) => {
+    setTimeout(() => {
+        const modalText = isVictory ? `You found the word:` : `The correct word was: `
+        gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`
+        gameModal.querySelector("h4").innerText = `${isVictory ? 'Congrats!' : 'Game Over!'}`
+        gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+        gameModal.classList.add("show");
+    },300)
 }
 
 const initGame = (button, clickedLetter) => {
     if(currentWord.includes(clickedLetter)) {
         [...currentWord].forEach((letter, index) => {
             if(letter === clickedLetter) {
+                correctLetters.push(letter);
                 wordDisplay.querySelectorAll("li")[index].innerText = letter;
                 wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
             }
         })
     } else {
         wrongGuessCount++;
-        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`
     }
 
     button.disabled = true;
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    if(wrongGuessCount === maxGuesses) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
 }
 
 // 키보드 버튼 만드는 for문
@@ -41,3 +64,4 @@ for (let i = 97; i <= 122; i++) {
 }
 
 getRandomWord();
+playAgainBtn.addEventListener("click" , getRandomWord)
