1b3ada3cfb34e402095e4582e3af6a5cda6d3482
choitjddn0311 <cswcjw311427@naver.com>
2025-01-18 14:22:17 +0900

    feat : text value null fix

--- To_do_list/todo.js
+++ To_do_list/todo.js
@@ -6,18 +6,18 @@ plus.addEventListener('click' , ()=> {
     let li = document.createElement('li');
     let del = document.createElement('button');
 
-    del.textContent = 'delete';
-    del.addEventListener('click' , () => {
-        todo.removeChild(li);
-    });
-
-    li.textContent = text.value;
-    li.appendChild(del);
-    todo.appendChild(li);
-    text.value = '';
-    if(text.value.trim() === '') {
-        alert("the values are empty , please enter the values");
-        return;
+    if(text.value == ''){
+        alert("please input the todo list");
     }
-})
+    else {
+        del.textContent = 'finish';
+        del.addEventListener('click' , () => {
+            todo.removeChild(li);
+        });
 
+        li.textContent = text.value;
+        li.appendChild(del);
+        todo.appendChild(li);
+        text.value = '';
+    }
+});
