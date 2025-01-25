edbe4258f3fe8d7b77248bc5ac6101c0474a59f0
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-01-25 23:16:58 +0900

    feat: 필요없는 코드 삭제

--- scroll_event/application/script.js
+++ scroll_event/application/script.js
@@ -3,9 +3,6 @@ window.addEventListener('scroll' , () => {
     const scrollTop = document.querySelector('html').scrollTop;
     const scrollHeight = document.querySelector('html').scrollHeight;
     const clientHeight = document.querySelector('html').clientHeight;
-    console.log("scrollTop: " , scrollTop);
-    console.log("scrollHeight:" , scrollHeight);
-    console.log("clientHeight:" , clientHeight);
 
     const progress = ((scrollTop+clientHeight) / scrollHeight) * 100;
 
