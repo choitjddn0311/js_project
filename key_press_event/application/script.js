cc2c0a28699e1ab3de09ac04af81fd481b196c25
choitjddn0311 <cswcjw311427@naver.com>
2025-02-03 10:58:45 +0900

    feat: 키보드 이벤트를 활용한 기초 계산기기

---
+++ key_press_event/application/script.js
@@ -0,0 +1,25 @@
+const showBox = document.querySelector('.showBox');
+const num = document.querySelectorAll('.num');
+const keypress = () => {
+    window.addEventListener('keydown' , (e) => {
+        const key = document.getElementById(e.key);
+        if(key) {
+            showBox.innerHTML = `<p>${e.key}</p>`;
+            key.classList.add('press');
+        }
+    })
+    window.addEventListener('keyup' , (e) => {
+        const key = document.getElementById(e.key);
+        if(key) {
+            key.classList.remove('press');
+        }
+    })
+}
+//에러
+const numpress = () => {
+    num.addEventListener('click' , () => {
+        showBox.innerHTML = num.value;
+    })
+}
+keypress();
+numpress();
