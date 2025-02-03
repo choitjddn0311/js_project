fd2e33daea153eb78812d8111c36921da97aa67a
choitjddn0311 <cswcjw311427@naver.com>
2025-02-03 18:06:48 +0900

    feat: 숫자 누적형으로 변경 및 길이 제한한

--- key_press_event/application/script.js
+++ key_press_event/application/script.js
@@ -4,7 +4,12 @@ const keypress = () => {
     window.addEventListener('keydown' , (e) => {
         const key = document.getElementById(e.key);
         if(key) {
-            showBox.innerHTML = `<p>${e.key}</p>`;
+            const currentItem = showBox.querySelectorAll('p').length;
+            if(currentItem==10){
+                alert("니 숫자 너무 많이 씀 ㅅㄱ")
+                return;
+            }
+            showBox.innerHTML += `<p>${e.key}</p>`;
             key.classList.add('press');
         }
     })
