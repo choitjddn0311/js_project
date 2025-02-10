59138f778f613a2e84ea0bee5dea3164dd31913f
choitjddn0311 <cswcjw311427@naver.com>
2025-02-10 15:07:31 +0900

    feat: error

--- To_do_list/refactoring/script.js
+++ To_do_list/refactoring/script.js
@@ -1,6 +1,7 @@
 const listBox = document.getElementById('listBox');
 const myList = document.getElementById('myList');
 const addBtn = document.getElementById('addBtn');
+const modal = document.getElementById('modal');
 const listAdd = () => {
     addBtn.addEventListener('click' , () => {
         if(myList.value == ''){
@@ -12,23 +13,28 @@ const listAdd = () => {
         if(noChildText){
             noChildText.remove();
         }
-
+        
         const listMax = Math.floor(listBox.clientHeight/40);
         if(listBox.children.length <= listMax) {
             listBox.innerHTML += `
             <li class="list"><p>${myList.value}</p> <input type="checkbox" class="listCK"></li>
-        `;
+            `;
         } else if(listBox.children.length > listMax) {
             alert('더이상 추가할 수 없습니다.');
         }
     })
 }
-const listModal = () => {
-    listBox.addEventListener('click' , (e) => {
-        if(e.target.closest('li')) {
-        }
-    })
-}
+// const listModal = () => {
+//     const closeBtn = document.getElementById('closeBtn');
+//     listBox.addEventListener('click' , (e) => {
+//         if(e.target.closest('li')) {
+//             modal.classList.add('showModal');
+//         }
+//     })
+//     closeBtn.addEventListener('click' , () => {
+//         modal.classList.remove('showModal');
+//     })
+// }
 const ChildCk = () => {
     if(listBox.children.length === 0){
     listBox.innerHTML = `<h1 class="noChild">오늘의 일정이 없습니다.</h1>`
@@ -37,4 +43,4 @@ const ChildCk = () => {
 
 ChildCk();
 listAdd();
-listModal();
+// listModal();
