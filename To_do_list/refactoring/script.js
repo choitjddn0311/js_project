ba8466e0e6dfe27fca444fa1a5f4be1a02128c2f
choitjddn0311 <cswcjw311427@naver.com>
2025-02-10 10:48:41 +0900

    feat: 자식요소 없을때와 있을때 나누어 다르게 출력 input값이 비었을때 입력되지않게 설정 list의 max개수 설정정

---
+++ To_do_list/refactoring/script.js
@@ -0,0 +1,38 @@
+const listBox = document.getElementById('listBox');
+const myList = document.getElementById('myList');
+const addBtn = document.getElementById('addBtn');
+const listAdd = () => {
+    addBtn.addEventListener('click' , () => {
+        if(myList.value == ''){
+            alert('할 일을 작성해주세요.');
+            myList.focus();
+            return;
+        }
+
+        const noChildText = document.querySelector('.noChild');
+        if(noChildText){
+            noChildText.remove();
+        }
+
+        const listMax = Math.floor(listBox.clientHeight/40);
+        console.log(listMax);
+        if(listBox.children.length <= listMax) {
+            listBox.innerHTML += `
+            <li><p>${myList.value}</p> <input type="checkbox" class="listCK"></li>
+        `;
+        } else if(listBox.children.length > listMax) {
+            alert('더이상 추가할 수 없습니다.');
+        }
+        
+
+
+    })
+}
+const ChildCk = () => {
+    if(listBox.children.length === 0){
+    listBox.innerHTML = `<h1 class="noChild">오늘의 일정이 없습니다.</h1>`
+    }
+};
+
+ChildCk();
+listAdd();
